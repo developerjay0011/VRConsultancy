3e1d65ed436598f6064c99fcfa5d16721aa4586e
Jay Dhimar <jaydhimar1@gmail.com>
2025-04-02 11:40:19 +0530

    Update LoanInquiryForm.tsx

--- src/components/LoanInquiry/LoanInquiryForm.tsx
+++ src/components/LoanInquiry/LoanInquiryForm.tsx
@@ -52,44 +52,6 @@ const schema = yup.object({
 
 type FormData = yup.InferType<typeof schema>
 
-const indianStates = [
-  'Andaman and Nicobar Islands',
-  'Andhra Pradesh',
-  'Arunachal Pradesh',
-  'Assam',
-  'Bihar',
-  'Chandigarh',
-  'Chhattisgarh',
-  'Dadra and Nagar Haveli and Daman and Diu',
-  'Delhi',
-  'Goa',
-  'Gujarat',
-  'Haryana',
-  'Himachal Pradesh',
-  'Jammu and Kashmir',
-  'Jharkhand',
-  'Karnataka',
-  'Kerala',
-  'Ladakh',
-  'Lakshadweep',
-  'Madhya Pradesh',
-  'Maharashtra',
-  'Manipur',
-  'Meghalaya',
-  'Mizoram',
-  'Nagaland',
-  'Odisha',
-  'Puducherry',
-  'Punjab',
-  'Rajasthan',
-  'Sikkim',
-  'Tamil Nadu',
-  'Telangana',
-  'Tripura',
-  'Uttar Pradesh',
-  'Uttarakhand',
-  'West Bengal'
-].sort()
 
 export default function LoanInquiryForm() {
   const [isSubmitting, setIsSubmitting] = useState(false)
